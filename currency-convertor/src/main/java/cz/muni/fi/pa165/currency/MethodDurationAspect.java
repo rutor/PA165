d9086a504fe3c96e7132785061c6d5e88af10569
Tomas Rudolf <xrudolf@nymfe72.fi.muni.cz>
2018-10-08 19:55:26 +0200

    zbyva bod 9

--- currency-convertor/src/main/java/cz/muni/fi/pa165/currency/MethodDurationAspect.java
+++ currency-convertor/src/main/java/cz/muni/fi/pa165/currency/MethodDurationAspect.java
@@ -1,11 +1,13 @@
 package cz.muni.fi.pa165.currency;
 
+
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 
 @Aspect
 public class MethodDurationAspect {
     @Around("execution(public * *(..))")
-    public void methodDuration(ProceedingJoinPoint joinPoint) {
+    public void methodDuration(ProceedingJoinPoint joinPoint) throws Throwable {
         long start = System.currentTimeMillis();
         joinPoint.proceed();
         long end = System.currentTimeMillis();
